aabc6cfa72632d8f4dd37519c935a87714d68ee1
Christian Uwaezuoke Ogbonna <xtianogbonna@gmail.com>
2022-12-08 20:01:15 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly and that piece of art is useful
+ *  main - Entry
  *
- *  Return: Always 0 (Success)
+ *  Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n" 59);
 	return (1);
-}
+ }
 	
