5873d60c629702060f9e571d2314b3f4eb9df341
Christian Uwaezuoke Ogbonna <xtianogbonna@gmail.com>
2022-12-08 16:31:32 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+
+
 /**
  *  main - Entry
+ *
+ *  Return: Always 0 (Success)
+ *
  */
+
 int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
+}
